1b9107015ba16ae30105862063dca8bcd8ccb0e7
Thomas Stege <support@tommis-tippspiel.de>
2018-09-28 22:35:17 +0200

    Project updated

--- websoccer/admin/config/imprint.php
+++ websoccer/admin/config/imprint.php
@@ -1,6 +1,1 @@
-Thomas Stege
-Leipziger Straße 60
-10117 Berlin
-Tel.: 0171-1978125
-<a href="mailto:support@tommis-tippspiel.de">Email: support [AT] tommis-tippspiel.de
-<a href="https://tommis-manager.tommis-tippspiel.de">Webseite: https://tommis-manager.tommis-tippspiel.de</a>
+n.a.
